636cff20590b05de2dd7fe27be174feb492226e2
GinYuki <fknight886@gmail.com>
2026-06-23 10:52:54 +0800

    remove py and add to gitignore

--- py/my_list = [1,2,3,4,5].py
+++
@@ -1,8 +0,0 @@
-my_list = [1,2,3,4,5]
-my_string = "Hello World"
-
-reversed_list_1 = my_list[::-1]
-reversed_string_1 = my_string[::-1]
-
-print(f"{reversed_list_1}'\n'")
-print(f"{reversed_string_1}")
